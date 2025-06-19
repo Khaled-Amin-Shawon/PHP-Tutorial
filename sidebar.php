70bdeb3601951687c748ec1bf34be8438069bdc4
Khaled-Amin-Shawon <mdkhaledaminshawon@gmail.com>
2025-06-20 05:09:00 +0600

    Add sidebar.php for navigation links to PHP tutorials and syntax.php for PHP syntax explanations, including Bangla translations.

---
+++ sidebar.php
@@ -0,0 +1,26 @@
+<?php /* sidebar.php */ ?>
+<div class="sidebar">
+    <nav>
+        <a href="index.php">PHP HOME</a>
+        <a href="intro.php">PHP Intro</a>
+        <a href="install.php">PHP Install</a>
+        <a href="syntax.php">PHP Syntax</a>
+        <a href="comments.php">PHP Comments</a>
+        <a href="variables.php">PHP Variables</a>
+        <a href="echo-print.php">PHP Echo / Print</a>
+        <a href="datatypes.php">PHP Data Types</a>
+        <a href="strings.php">PHP Strings</a>
+        <a href="numbers.php">PHP Numbers</a>
+        <a href="casting.php">PHP Casting</a>
+        <a href="math.php">PHP Math</a>
+        <a href="constants.php">PHP Constants</a>
+        <a href="magic-constants.php">PHP Magic Constants</a>
+        <a href="operators.php">PHP Operators</a>
+        <a href="if-else.php">PHP If...Else...Elseif</a>
+        <a href="switch.php">PHP Switch</a>
+        <a href="loops.php">PHP Loops</a>
+        <a href="functions.php">PHP Functions</a>
+        <a href="arrays.php">PHP Arrays</a>
+    </nav>
+</div>
+<?php /* End sidebar.php */ ?> 
---
+++ syntax.php
@@ -0,0 +1,18 @@
+<?php include 'header.php'; ?>
+<?php include 'sidebar.php'; ?>
+<div class="content">
+    <header>
+        <h1>PHP Syntax</h1>
+    </header>
+    <main>
+        <section class="section" id="syntax">
+            <h2>PHP Syntax</h2>
+            <p>All PHP code is written inside <code>&lt;?php ... ?&gt;</code> tags. Statements end with a semicolon <code>;</code>.</p>
+            <pre>&lt;?php
+    // PHP code goes here
+    echo "Hello, World!";
+?&gt;</pre>
+            <p class="bangla">সব PHP কোড <code>&lt;?php ... ?&gt;</code> ট্যাগের মধ্যে লিখতে হয় এবং প্রতিটি স্টেটমেন্ট শেষে <code>;</code> দিতে হয়।</p>
+        </section>
+    </main>
+<?php include 'footer.php'; ?> 
